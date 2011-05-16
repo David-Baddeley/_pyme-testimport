386909b34e8a211a2b5a76bdf261139470d9e188
David <David@David-PC>
2011-05-17 11:38:32 +1200

    various changes related to drift fitting & Zeiss

---
+++ Analysis/Models/spark.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+##################
+# spark.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+from scipy import *
+
+def sparkMod(p, X,T):
+    maxP, Td, Tr, x0, t0, sig0, b = p
+
+    f = ((T - t0) > 0)*maxP*exp(-(T-t0)/Td)*(1 - exp(-(T-t0)/Tr))*exp(-((X-x0)**2)/(2*(sig0*(1 + (((T - t0) > 0)*(T - t0))**.25)**2)))+ b
+
+    return f
---
+++ FileUtils/readTiff.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+##################
+# readTiff.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+import Image
+import numpy as np
+from PYME.misc import TiffImagePlugin #monkey patch PIL with improved tiff support from Priithon
+
+
+
+def read3DTiff(filename):
+    im = Image.open(filename)
+
+    im.seek(0)
+
+    #PIL's endedness support is subtly broken - try to fix it
+    #NB this is untested for floating point tiffs
+    endedness = 'LE'
+    if im.ifd.prefix =='MM':
+        endedness = 'BE'
+    
+
+    #ima = np.array(im.getdata(), 'int16').newbyteorder('BE')
+    ima = np.array(im.getdata()).newbyteorder(endedness)
+
+    print ima.dtype
+    
+    #print ima.shape
+
+    ima = ima.reshape((im.size[1], im.size[0], 1))
+
+    pos = im.tell()
+
+    try:
+        while True:
+            pos += 1
+            im.seek(pos)
+
+            #ima = np.concatenate((ima, np.array(im.getdata(), 'int16').newbyteorder('BE').reshape((im.size[1], im.size[0], 1))), 2)
+            ima = np.concatenate((ima, np.array(im.getdata()).newbyteorder(endedness).reshape((im.size[1], im.size[0], 1))), 2)
+
+    except EOFError:
+        pass
+
+    return ima
