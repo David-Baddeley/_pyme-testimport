12a401375c84df3cc3c65876c359e222d4863ad8
David Baddeley <d.baddeley@auckland.ac.nz>
2011-07-14 13:57:24 +1200

    - 1st steps in generalising colocalisation

---
+++ DSView/displayOptions.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+##################
+# displayOptions.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+from matplotlib import cm
+import numpy as np
+#import tables
+
+from PYME.DSView import dataWrap
+
+def fast_grey(data):
+    return data[:,:,None]*np.ones((1,1,4))
+
+fast_grey.name = 'fastGrey'
+     
+
+class DisplayOpts(object):
+    UPRIGHT, ROT90 = range(2)
+    SLICE_XY, SLICE_XZ, SLICE_YZ = range(3)
+
+    def __init__(self, datasource, xp=0, yp=0, zp=0, aspect=1):
+        self.WantChangeNotification = []
+        
+        self.Chans = []
+        self.Gains = []
+        self.Offs = []
+        self.cmaps = []
+        self.names = []
+        
+        self.xp=0
+        self.yp=0
+
+        self._zp=0
+
+        self.SetDataStack(datasource)
+        self.SetAspect(aspect)
+
+
+        self.orientation = self.UPRIGHT
+        self.slice = self.SLICE_XY
+        self.scale = 1.0
+
+    @property
+    def zp(self):
+        return self._zp
+
+    @zp.setter
+    def zp(self, value):
+        self._zp = value
+        #print 'z changed'
+        self.OnChange()
+
+    def SetDataStack(self, datasource):
+        self.ds = dataWrap.Wrap(datasource) #make sure data is wrapped
+
+        nchans = self.ds.shape[3]
+
+        if not nchans == len(self.Chans):
+            if nchans == 1:
+                self.Chans = [0]
+                self.Gains = [1]
+                self.Offs = [0]
+                self.cmaps = [fast_grey]
+            else:
+                self.Chans = []
+                self.Gains = []
+                self.Offs = []
+                self.cmaps = []
+
+                cms = [cm.r, cm.g, cm.b]
+
+                for i in range(nchans):
+                    self.Chans.append(i)
+                    self.Gains.append(1.)
+                    self.Offs.append(0.)
+                    self.cmaps.append(cms[i%len(cms)])
+
+        self.names = ['Chan %d' %i for i in range(nchans)]
+
+        self.OnChange()
+
+    def SetGain(self, chan, gain):
+        self.Gains[chan] = gain
+        self.OnChange()
+
+    def SetOffset(self, chan, offset):
+        self.Offs[chan] = offset
+        self.OnChange()
+
+    def SetCMap(self, chan, cmap):
+        self.cmaps[chan] = cmap
+        self.OnChange()
+
+    def SetOrientation(self, orientation):
+        self.orientation = orientation
+        self.OnChange()
+
+    def SetSlice(self, slice):
+        self.slice = slice
+        self.OnChange()
+
+    def SetAspect(self, aspect):
+        if np.isscalar(aspect):
+            self.aspect = [1., 1., aspect]
+        elif len(aspect) == 3:
+            self.aspect = aspect
+        else:
+            self.aspect = [1., 1., 1.]
+            
+        self.OnChange()
+
+    def SetScale(self, scale):
+        self.scale = scale
+        self.OnChange()
+
+    def OnChange(self):
+        for fcn in self.WantChangeNotification:
+            fcn()
+
+    def Optimise(self):
+        if len(self.ds.shape) == 2:
+            self.Offs[0] = 1.*self.ds.min()
+            self.Gains[0] =1./(self.ds.max()- self.ds.min())
+        elif len(self.ds.shape) ==3:
+            self.Offs[0] = 1.*self.ds[:,:,self.zp].min()
+            self.Gains[0] =1./(self.ds[:,:,self.zp].max()- self.ds[:,:,self.zp].min())
+        else:
+            for i in range(len(self.Chans)):
+                self.Offs[i] = self.ds[:,:,self.zp,self.Chans[i]].min()
+                self.Gains[i] = 1.0/(self.ds[:,:,self.zp,self.Chans[i]].max() - self.Offs[i])
+
+        self.OnChange()
---
+++ SampleDB/samples/templatetags/extra_filters.py
@@ -0,0 +1,58 @@
+from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+def is_changed(value): # Only one argument.
+    #is_changed.oldValue=None
+    #is_changed.currentState=False
+
+    #print value, is_changed.oldValue
+
+    #while True:
+    if not (value == is_changed.oldValue):
+        is_changed.currentState = not is_changed.currentState
+
+    is_changed.oldValue = value
+
+    if is_changed.currentState:
+        return 'odd'
+    else:
+        return 'even'
+
+is_changed.oldValue=None
+is_changed.currentState=False
+
+@register.filter
+def sum(value, arg): 
+    s = 0
+    #print value
+    for v in value:
+        s += v.__getattribute__(arg)
+        #print s
+
+    return s
+
+@register.filter
+@stringfilter
+def tagify(value, autoescape=None):
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+        
+    s = value
+    if value.startswith('Protocol_'):
+        s = '<b>P</b> %s' % esc(value.split('_')[1])
+
+    if value.startswith('FitModule_'):
+        s = '<b>F</b> %s' % esc(value.split('_')[1])
+
+    return mark_safe(s)
+
+tagify.needs_autoescape=True
+
+    
