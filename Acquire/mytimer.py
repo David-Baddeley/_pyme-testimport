05efc74ccdd4aa0626c0e2d9339031adec3a7429
David <David@David-PC.fmhs.auckland.ac.nz>
2011-08-16 15:24:02 +1200

    data source fix for .lsm files

---
+++ Acquire/mytimer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+##################
+# mytimer.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+import wx
+
+class mytimer(wx.Timer):
+    def __init__(self):
+        wx.Timer.__init__(self)
+        self.WantNotification = []
+
+    def Notify(self):
+        for a in self.WantNotification:
+                a()
