f205d3e8a4f53e47a59cb0ffa4b14e185ab7fe49
David Baddeley <David Baddeley@localhost>
2011-10-03 18:24:09 +1300

    roi fixes and misc refactoring

---
+++ Acquire/eventLog.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+##################
+# eventLog.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+WantEventNotification = []
+
+def logEvent(eventName, eventDescr = ''):
+    for evl in WantEventNotification:
+            evl.logEvent(eventName, eventDescr)
