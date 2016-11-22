550997aceeced879cc4cb034c7642dc5f27073fa
kenny <kenny@localhost>
2016-11-22 16:50:12 -0500

    fix to metadata editing for filename parameters

---
+++ PYME/recipes/inputoutput.py
@@ -0,0 +1,8 @@
+from .base import register_module, ModuleBase, Filter, Float, Enum, CStr, Bool, Int
+from traits.api import DictStrStr
+import numpy as np
+import pandas as pd
+from PYME.IO import tabular
+
+
+
---
+++ PYME/version.py
@@ -0,0 +1,5 @@
+#PYME uses date based versions (yy.m.d)
+version = '16.11.10'
+
+#Mercurial changeset id
+changeset = '60c9379235f9+'
