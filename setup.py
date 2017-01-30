98135615e6da05bcbcefa8ea388b5a7a9ee31a44
Andrew Barentine <andrew.barentine@yale.edu>
2017-01-30 15:46:37 -0500

    fixing bug in astigGaussFitFR where x and y sigmas were swapped from x and y positions

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+##################
+# setup.py
+#
+# Copyright David Baddeley, 2009
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+import setuptools #to monkey-patch distutils for ms visualc for python
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('PYME',parent_package,top_path)
+    config.add_subpackage('PYME')
+    
+    #config.make_svn_version_py()  # installs __svn_version__.py
+    #config.make_config_py()
+    config.get_version('PYME/version.py')
+    return config
+
+entry_points={
+    'console_scripts': [
+        'PYMEBatch = PYME.Analysis.Modules.batchProcess:main',
+        'taskServerZC = PYME.ParallelTasks.taskServerZC:main',
+        'taskWorkerZC = PYME.ParallelTasks.taskWorkerZC:main',
+        'PYMEDataServer = PYME.ParallelTasks.HTTPDataServer:main',
+        'PYMEClusterDup = PYME.io.clusterDuplication:main',
+        'PYMEDistributor = PYME.ParallelTasks.PYMEDistributor',
+        'PYMEscmosmapgen = PYME.Analysis.gen_sCMOS_maps:main',
+        'PYMENodeServer = PYME.ParallelTasks.PYMENodeServer',
+        'dh5view = PYME.DSView.dsviewer:main',
+        'PYMEAcquire = PYME.Acquire.PYMEAcquire:main',
+        'VisGUI = PYME.LMVis.VisGUI:main',
+        'launchWorkers = PYME.ParallelTasks.launchWorkers:main',
+        #'taskServerZC = PYME.ParallelTasks.taskServerZC:main',
+        #'taskWorkerZC = PYME.ParallelTasks.taskWorkerZC:main',
+        'fitMonP = PYME.ParallelTasks.fitMonP:main',
+        'bakeshop = PYME.recipes.bakeshop:main',
+    ]
+}
+
+if __name__ == '__main__':
+    import setuptools
+    from numpy.distutils.core import setup
+    conf = configuration(top_path='').todict()
+    conf['entry_points'] = entry_points
+    setup(**conf)
