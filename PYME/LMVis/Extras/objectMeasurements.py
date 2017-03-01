269fde6ffcd1bf28fca53ec7b1a91cc346dc3966
Andrew Barentine <andrew.barentine@yale.edu>
2017-03-01 13:59:09 -0500

    adding autoZBounds option for density mapping recipe module

---
+++ PYME/LMVis/Extras/objectMeasurements.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+##################
+# objectMeasurements.py
+#
+# Copyright David Baddeley, 2011
+# d.baddeley@auckland.ac.nz
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##################
+
+import wx
+import numpy as np
+
+class ParticleTracker:
+    def __init__(self, visFr):
+        self.visFr = visFr
+
+        visFr.AddMenuItem('Extras', "Get segmented IDs from image", self.OnGetIDs)
+        visFr.AddMenuItem('Extras', "Measure objects", self.OnMeasure)
+
+
+    def OnGetIDs(self, event):
+        from PYME.IO import image
+
+        visFr = self.visFr
+        pipeline = visFr.pipeline
+
+        dlg = wx.SingleChoiceDialog(
+                None, 'choose the image which contains labels', 'Use Segmentation',
+                image.openImages.keys(),
+                wx.CHOICEDLG_STYLE
+                )
+
+        if dlg.ShowModal() == wx.ID_OK:
+            img = image.openImages[dlg.GetStringSelection()]
+            
+            #account for ROIs
+            dRx = pipeline.mdh['Camera.ROIPosX']*pipeline.mdh['voxelsize.x']*1e3 - img.mdh['Camera.ROIPosX']*img.mdh['voxelsize.x']*1e3
+            dRy = pipeline.mdh['Camera.ROIPosY']*pipeline.mdh['voxelsize.y']*1e3 - img.mdh['Camera.ROIPosY']*img.mdh['voxelsize.y']*1e3
+
+            pixX = np.round((pipeline.mapping['x'] - img.imgBounds.x0 - dRx)/img.pixelSize).astype('i')
+            pixY = np.round((pipeline.mapping['y'] - img.imgBounds.y0 - dRy)/img.pixelSize).astype('i')
+
+            ind = (pixX < img.data.shape[0])*(pixY < img.data.shape[1])*(pixX >= 0)*(pixY >= 0)
+
+            ids = np.zeros_like(pixX)
+            #assume there is only one channel
+            ids[ind] = img.data[:,:,:,0].squeeze()[pixX[ind], pixY[ind]].astype('i')
+
+            numPerObject, b = np.histogram(ids, np.arange(ids.max() + 1.5) + .5)
+
+            pipeline.addColumn('objectID', ids)
+            pipeline.addColumn('NEvents', numPerObject[ids-1])
+
+            pipeline.Rebuild()
+
+        dlg.Destroy()
+
+    def OnMeasure(self, event):
+        from PYME.LMVis import objectMeasure
+
+        pipeline = self.visFr.pipeline
+
+        chans = pipeline.colourFilter.getColourChans()
+
+        ids = set(pipeline.mapping['objectID'].astype('i'))
+        pipeline.objectMeasures = {}
+
+        if len(chans) == 0:
+            pipeline.objectMeasures['Everything'] = objectMeasure.measureObjectsByID(pipeline.colourFilter, 10,ids)
+        else:
+            curChan = pipeline.colourFilter.currentColour
+
+            chanNames = chans[:]
+
+#            if 'Sample.Labelling' in metadata.getEntryNames():
+#                lab = metadata.getEntry('Sample.Labelling')
+#
+#                for i in range(len(lab)):
+#                    if lab[i][0] in chanNames:
+#                        chanNames[chanNames.index(lab[i][0])] = lab[i][1]
+
+            for ch, i in zip(chans, range(len(chans))):
+                pipeline.colourFilter.setColour(ch)
+                #fitDecayChan(colourFilter, metadata, chanNames[i], i)
+                pipeline.objectMeasures[chanNames[i]] = objectMeasure.measureObjectsByID(pipeline.colourFilter, 10,ids)
+            pipeline.colourFilter.setColour(curChan)
+
+
+
+
+
+def Plug(visFr):
+    """Plugs this module into the gui"""
+    ParticleTracker(visFr)
